a94136ae0c1255ea224ec348b94ea80b5ca56020
WilliamReisO <oliveira._r@hotmail.com>
2023-09-29 13:12:22 -0300

    Escopo de classe

---
+++ Escopo/conta.java
@@ -0,0 +1,36 @@
+package Escopo;
+
+public class conta {
+    
+    //varia el da classe conta 
+    double saldo=10.0;
+    private char[] novoSaldo;
+
+    public void sacar (Double valor){
+        // variavel local de método
+        double novoSaldo = saldo - valor;
+    }
+
+    public void imprimirSaldo() {
+        //disponível em toda classe 
+
+        System.out.println(saldo);
+
+        //somente o método sacar conhece essa variavel 
+
+        System.out.println(novoSaldo);
+    }
+    public double calcularDividaExponencial(){
+        // variavel local de método
+
+        double valorParcela = 50.0;
+        double valorMontante = 0.0; // começando a variavel 
+        for(int x=1;x<=5;x++){
+            // variavel que se reinicia a cada execução.
+            double valorCalculado = valorParcela * x;
+            valorMontante = valorMontante + valorCalculado;
+        }
+        return valorMontante;
+        
+    }
+}
