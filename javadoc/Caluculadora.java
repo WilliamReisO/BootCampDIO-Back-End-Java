160a8267887ffc1f0fdb8a420ec466ea5698ddb1
WilliamReisO <oliveira._r@hotmail.com>
2023-09-29 14:44:45 -0300

    comentarios utilidades é diferenças , criação de arquivo Ja vaDoc

---
+++ javadoc/Caluculadora.java
@@ -0,0 +1,28 @@
+package javadoc;
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora Realiza operações matemáticas em numeros inteiros
+ * 
+ * <b>Note:</b> Leia atentamenta a documentação desta classes para desfrutar dos
+ * recusrsos oferecidos pelo autor
+ * 
+ * @author Gleyso Sampio
+ * @verion 1.0
+ * @since 01/01/2022
+ */
+public class Caluculadora {
+
+    /*
+     * Este método é utilizado para somar ois numeros inteiros
+     * 
+     * @param numeroUm este é o primiiro parâmetro do método
+     * 
+     * @param numeroDois ete é o segundo parârametro do método
+     * 
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
